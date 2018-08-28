a73c326e60d7fb81df995989a25c01d3ff583655
KhoiNguyenICT <khoinguyenict@gmail.com>
2018-08-28 20:16:54 +0700

    Initialize

---
+++ Cuda.Model/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Cuda.Model.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cuda.Model
+{
+    public class AppDbContext: IdentityDbContext<Account, ApplicationRole, Guid>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Asset> Assets { get; set; }
+
+        public virtual DbSet<ConfigurationValue> ConfigurationValues { get; set; }
+    }
+}
---
+++ Cuda.Model/Entities/AccountRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Cuda.Model.Entities
+{
+    public class AccountRole : IdentityUserRole<Guid>
+    {
+        public Account Account { get; set; }
+
+        public ApplicationRole ApplicationRole { get; set; }
+    }
+}
